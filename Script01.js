42741da3a38ed29a7debc2a2cd8ee57d2420950b
Raajulogu <111479920+Raajulogu@users.noreply.github.com>
2023-02-20 10:17:59 +0530

    Delete Script01.js

--- Script01.js
+++
@@ -1,9 +0,0 @@
-var res=new XMLHttpRequest();
-res.open('GET',"https://restcountries.com/v2/all");
-res.send();
-res.onload=function(){
-    var result=JSON.parse(res.response);
-    for(i=0;i<result.length;i++){
-        console.log(result[i].flag);
-    }
-}
